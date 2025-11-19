d2af3728a4edb43789c219970e68c8d89fbfe61f
Gina Proaño <gina.proanio76@gmail.com>
2025-11-18 22:30:08 -0500

    cambios de pruebas16

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -4,7 +4,6 @@ from .user_routes import user_bp
 from .admin_routes import admin_bp
 from .master_routes import master_bp
 from .api_routes import api_bp
-from .dev_routes import dev_bp # <-- AÑADIR ESTA LÍNEA
 
 def init_app(app):
     app.register_blueprint(public_bp)
@@ -12,4 +11,3 @@ def init_app(app):
     app.register_blueprint(admin_bp)
     app.register_blueprint(master_bp)
     app.register_blueprint(api_bp)
-    app.register_blueprint(dev_bp) # <-- Y AÑADIR ESTA
